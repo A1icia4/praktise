4ed9512153729155e9d57628e94c6896e3e5a661
alicia <aliciareborn1@gmail.com>
2018-09-10 00:07:32 +0300

    git praktice

---
+++ README.md
@@ -0,0 +1,1 @@
+I spent minutes writing this code(haha) still practising with git anyway
---
+++ while.c
@@ -0,0 +1,10 @@
+ #include <stdio.h>
+int main(){
+
+  int i,a=10;
+
+  while(a<=50){
+    printf("%i\n", i+a);
+    a+=10;
+ } 
+  return 0;}
